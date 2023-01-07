8bec1ee9d9b2b1a3523332b71883c501d79a93d6
rhian-cs <rhian.luis.cs+github@gmail.com>
2023-01-07 00:04:10 -0300

    Lesson 3 - Collisions, move_and_slide, Lint files to use spaces instead of tabs

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,15 +2,19 @@ extends KinematicBody2D
 
 # Called when the node enters the scene tree for the first time.
 # func _ready():
-# 	print("Hello, world!")
+#   print("Hello, world!")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
+#  pass
 
 const PlayerVelocityCalculator = preload("PlayerVelocityCalculator.gd")
 onready var player_velocity_calculator = PlayerVelocityCalculator.new()
 
+var velocity = Vector2.ZERO
+
 func _physics_process(delta):
-	move_and_collide(player_velocity_calculator.calculate(delta))
+  velocity = player_velocity_calculator.calculate(velocity, delta)
+
+  velocity = move_and_slide(velocity)
--- Player/PlayerVelocityCalculator.gd
+++ Player/PlayerVelocityCalculator.gd
@@ -1,56 +1,51 @@
-var velocity = Vector2.ZERO
-
 var horizontal_velocity_override = 0
 var vertical_velocity_override = 0
 
-const ACCELERATION = 25
-const MAX_SPEED = 100
-const FRICTION = 15
+const ACCELERATION = 800
+const MAX_SPEED = 80
+const FRICTION = 800
 
-func calculate(delta):
-	var input_vector = Vector2.ZERO
+func calculate(prev_velocity, delta):
+  var input_vector = Vector2.ZERO
 
-	set_horizontal_velocity_override()
-	set_vertical_velocity_override()
+  set_horizontal_velocity_override()
+  set_vertical_velocity_override()
 
-	input_vector.x = calculate_horizontal_velocity()
-	input_vector.y = calculate_vertical_velocity()
+  input_vector.x = calculate_horizontal_velocity()
+  input_vector.y = calculate_vertical_velocity()
 
-	if(input_vector != Vector2.ZERO):
-		velocity += input_vector.normalized() * ACCELERATION * delta
-		velocity = velocity.clamped(MAX_SPEED * delta)
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-
-	return velocity
+  if(input_vector != Vector2.ZERO):
+    return prev_velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+  else:
+    return prev_velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 func set_horizontal_velocity_override():
-	if(Input.is_action_just_pressed("ui_left")):
-		horizontal_velocity_override = -1
-	if(Input.is_action_just_pressed("ui_right")):
-		horizontal_velocity_override =  1
+  if(Input.is_action_just_pressed("ui_left")):
+    horizontal_velocity_override = -1
+  if(Input.is_action_just_pressed("ui_right")):
+    horizontal_velocity_override =  1
 
 func set_vertical_velocity_override():
-	if(Input.is_action_just_pressed("ui_up")):
-		vertical_velocity_override = -1
-	if(Input.is_action_just_pressed("ui_down")):
-		vertical_velocity_override = 1
+  if(Input.is_action_just_pressed("ui_up")):
+    vertical_velocity_override = -1
+  if(Input.is_action_just_pressed("ui_down")):
+    vertical_velocity_override = 1
 
 
 func calculate_horizontal_velocity():
-	var right_strength = Input.get_action_strength("ui_right")
-	var left_strength = Input.get_action_strength("ui_left")
+  var right_strength = Input.get_action_strength("ui_right")
+  var left_strength = Input.get_action_strength("ui_left")
 
-	if(right_strength && left_strength):
-		return horizontal_velocity_override
+  if(right_strength && left_strength):
+    return horizontal_velocity_override
 
-	return right_strength - left_strength
+  return right_strength - left_strength
 
 func calculate_vertical_velocity():
-	var down_strength = Input.get_action_strength("ui_down")
-	var up_strength = Input.get_action_strength("ui_up")
+  var down_strength = Input.get_action_strength("ui_down")
+  var up_strength = Input.get_action_strength("ui_up")
 
-	if(down_strength && up_strength):
-		return vertical_velocity_override
+  if(down_strength && up_strength):
+    return vertical_velocity_override
 
-	return down_strength - up_strength
+  return down_strength - up_strength
