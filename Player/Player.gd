4a487bcf972f93d3aec4519b859a918060b2becd
rhian-cs <rhian.luis.cs+github@gmail.com>
2023-01-08 01:45:17 -0300

    Lesson 9 - Attacking Animation and State Machines

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,16 +1,5 @@
 extends KinematicBody2D
 
-# Called when the node enters the scene tree for the first time.
-# func _ready():
-#   print("Hello, world!")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#  pass
-
-var velocity = Vector2.ZERO
-
 const PlayerMovementCalculator = preload("PlayerMovementCalculator.gd")
 const PlayerAnimationProcessor = preload("PlayerAnimationProcessor.gd")
 
@@ -19,13 +8,41 @@ onready var animation_tree: AnimationTree = $AnimationTree
 onready var movement_calculator = PlayerMovementCalculator.new()
 onready var animation_processor = PlayerAnimationProcessor.new(animation_tree)
 
-func _physics_process(delta):
+enum {
+  MOVE = 10,
+  ROLL = 20,
+  ATTACK = 30
+}
+
+var velocity = Vector2.ZERO
+var state = MOVE
+
+func _physics_process(delta: float):
+  match(state):
+    MOVE:
+      move_state(delta)
+    ROLL:
+      pass
+    ATTACK:
+      attack_state()
+
+func move_state(delta: float):
   var movement_result = movement_calculator.calculate(velocity, delta)
 
   var input_vector = movement_result[0]
   var is_moving = movement_result[1]
   var new_velocity = movement_result[2]
 
-  animation_processor.process(input_vector, is_moving)
+  animation_processor.process_movement(input_vector, is_moving)
 
   velocity = move_and_slide(new_velocity)
+
+  if Input.is_action_just_pressed("attack"):
+    state = ATTACK
+
+func attack_state():
+  velocity = Vector2.ZERO
+  animation_processor.process_attack()
+
+func on_attack_animation_finished():
+  state = MOVE
--- Player/PlayerAnimationProcessor.gd
+++ Player/PlayerAnimationProcessor.gd
@@ -1,15 +1,22 @@
 var animation_tree: AnimationTree
 var animation_state: AnimationNodeStateMachinePlayback
+var animation_player: AnimationPlayer
 
 func _init(player_animation_tree: AnimationTree):
   animation_tree = player_animation_tree
+  animation_tree.active = true
+
   animation_state = animation_tree.get("parameters/playback")
 
-func process(input_vector, is_moving):
+func process_movement(input_vector, is_moving):
   if(is_moving):
     animation_tree.set("parameters/Idle/blend_position", input_vector)
     animation_tree.set("parameters/Run/blend_position", input_vector)
+    animation_tree.set("parameters/Attack/blend_position", input_vector)
 
     animation_state.travel("Run")
   else:
     animation_state.travel("Idle")
+
+func process_attack():
+  animation_state.travel("Attack")
